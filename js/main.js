4b0e36b71a99dc4a6339a082b40a419893209bee
Felipe Alan <felipealanbl@gmail.com>
2021-07-17 02:00:55 -0300

    commit versao final

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$cadastrar = document.querySelector("#cadastrar")
+$emailField = document.querySelector('.email-field')
+
+
+
+$cadastrar.addEventListener('click', (e) => {
+    registrarEmail($emailField.value)
+})
+
+
+function registrarEmail (email) {
+
+    if(!validateEmail($emailField.value)) {
+        alert('Por favor, insira um e-mail válido')
+    } else {
+        console.log(email)
+        localStorage.setItem('email', email)
+    }
+}
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
